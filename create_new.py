a21a28aa45912816ce3f15236259175f8488ab08
Kevin Stradtman <stradtkt22@gmail.com>
2020-01-23 19:13:49 -0500

    Added and image and some code

--- create_new.py
+++ create_new.py
@@ -4,6 +4,10 @@ from PIL import  ImageTk, Image
 root = Tk()
 root.title("Learn to code at gowebkit.com")
 root.geometry("600x300")
+root.iconbitmap("c:/gui/gowebkit.ico")
 
+top = Toplevel()
+my_img = ImageTk.PhotoImage(Image.open("images/aspen.png"))
+my_label = Label(top, image=my_img).pack()
 
 mainloop()
