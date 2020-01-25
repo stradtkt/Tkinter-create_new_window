242f2ae478d1d74e02f3ef745d21c6d498816c77
Kevin Stradtman <stradtkt22@gmail.com>
2020-01-25 15:53:05 -0500

    Added some code

--- create_new.py
+++ create_new.py
@@ -6,8 +6,22 @@ root.title("Learn to code at gowebkit.com")
 root.geometry("600x300")
 root.iconbitmap("c:/gui/gowebkit.ico")
 
-top = Toplevel()
-my_img = ImageTk.PhotoImage(Image.open("images/aspen.png"))
-my_label = Label(top, image=my_img).pack()
+
+
+
+def open():
+    global  my_img
+    top = Toplevel()
+    top.title("Calculator")
+    top.geometry("1000x500")
+    top.iconbitmap("c:/gui/gowebkit.ico")
+    my_img = ImageTk.PhotoImage(Image.open("images/aspen.png"))
+    my_label = Label(top, image=my_img)
+    my_label.pack()
+    btn_close = Button(top, text="Close", command=top.destroy)
+    btn_close.pack()
+
+btn = Button(root, text="Open Second Window", command=open).pack()
+
 
 mainloop()
